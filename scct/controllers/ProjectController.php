352ceef7c986991597310c87939bb85f8c712dbd
aquiljohston <aquiljohnston3@gmail.com>
2016-01-13 15:34:41 -0500

    Added guest check on controllers to redirect to login

--- scct/controllers/ProjectController.php
+++ scct/controllers/ProjectController.php
@@ -33,6 +33,11 @@ class ProjectController extends Controller
      */
     public function actionIndex()
     {
+		//guest redirect
+		if (Yii::$app->user->isGuest)
+		{
+			return $this->redirect(['login/login']);
+		}
 		//RBAC permissions check
 		if (Yii::$app->user->can('viewProjectIndex'))
 		{
@@ -58,6 +63,11 @@ class ProjectController extends Controller
      */
     public function actionView($id)
     {
+		//guest redirect
+		if (Yii::$app->user->isGuest)
+		{
+			return $this->redirect(['login/login']);
+		}
 		//RBAC permissions check
 		if (Yii::$app->user->can('viewProject'))
 		{
@@ -78,6 +88,11 @@ class ProjectController extends Controller
      */
     public function actionCreate()
     {
+		//guest redirect
+		if (Yii::$app->user->isGuest)
+		{
+			return $this->redirect(['login/login']);
+		}
 		//RBAC permissions check
 		if (Yii::$app->user->can('createProject'))
 		{
@@ -105,6 +120,11 @@ class ProjectController extends Controller
      */
     public function actionUpdate($id)
     {
+		//guest redirect
+		if (Yii::$app->user->isGuest)
+		{
+			return $this->redirect(['login/login']);
+		}
 		//RBAC permissions check
 		if (Yii::$app->user->can('updateProject'))
 		{
@@ -132,6 +152,11 @@ class ProjectController extends Controller
      */
     public function actionDelete($id)
     {
+		//guest redirect
+		if (Yii::$app->user->isGuest)
+		{
+			return $this->redirect(['login/login']);
+		}
 		//RBAC permissions check
 		if (Yii::$app->user->can('deleteProject'))
 		{
