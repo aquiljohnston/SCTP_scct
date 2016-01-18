51147ffe31540ede57028ac88a38e1312f29fe56
unknown <tzhang@8l9z362.speardom.com>
2016-01-18 16:25:46 -0500

    updated views after moving project to the server.

--- scct/web/js/header.js
+++ scct/web/js/header.js
@@ -52,21 +52,21 @@ $(document).ready(function(){
 	var nav4 = $("<li class='dropdown'><a href='' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>"
         + "ADMINISTRATION<b class='caret'></b></a>"
         + "    <ul class='dropdown-menu' role='menu'>"  
-            + "<li><a data-description='Image Animation' href='http://localhost:8000/index.php?r=user%2Findex'>User Management</a></li>"
-            + "<li><a data-description='Image Animation' href='http://localhost:8000/index.php?r=equipment%2Findex'>Equipment Manager</a></li>"
-            + "<li><a data-description='Image Animation' href='http://localhost:8000/index.php?r=time-card%2Findex'>Timecards</a></li>"
-            + "<li><a data-description='Instrument Repair' href='http://localhost:8000/index.php?r=mileage-card%2Findex'>Mileagecards</a></li>"
+            + "<li><a data-description='Image Animation' href='http://scct.southerncrossinc.com/index.php?r=user%2Findex'>User Management</a></li>"
+            + "<li><a data-description='Image Animation' href='http://scct.southerncrossinc.com/index.php?r=equipment%2Findex'>Equipment Manager</a></li>"
+            + "<li><a data-description='Image Animation' href='http://scct.southerncrossinc.com/index.php?r=time-card%2Findex'>Timecards</a></li>"
+            + "<li><a data-description='Instrument Repair' href='http://scct.southerncrossinc.com/index.php?r=mileage-card%2Findex'>Mileagecards</a></li>"
         + "</ul></li>");
 		
     var nav5 = $("<li class='dropdown'><a href='' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>"
         + "ADMINISTRATION<b class='caret'></b></a>"
         + "    <ul class='dropdown-menu' role='menu'>"  
-			+ "<li><a data-description='Instrument Repair' href='http://localhost:8000/index.php?r=client%2Findex'>Clients</a></li>"
-			+ "<li><a data-description='Instrument Repair' href='http://localhost:8000/index.php?r=project%2Findex'>Projects</a></li>"			
-            + "<li><a data-description='Image Animation' href='http://localhost:8000/index.php?r=user%2Findex'>User Management</a></li>"
-            + "<li><a data-description='Image Animation' href='http://localhost:8000/index.php?r=equipment%2Findex'>Equipment Manager</a></li>"
-            + "<li><a data-description='Image Animation' href='http://localhost:8000/index.php?r=time-card%2Findex'>Timecards</a></li>"
-            + "<li><a data-description='Instrument Repair' href='http://localhost:8000/index.php?r=mileage-card%2Findex'>Mileagecards</a></li>"
+			+ "<li><a data-description='Instrument Repair' href='http://scct.southerncrossinc.com/index.php?r=client%2Findex'>Clients</a></li>"
+			+ "<li><a data-description='Instrument Repair' href='http://scct.southerncrossinc.com/index.php?r=project%2Findex'>Projects</a></li>"			
+            + "<li><a data-description='Image Animation' href='http://scct.southerncrossinc.com/index.php?r=user%2Findex'>User Management</a></li>"
+            + "<li><a data-description='Image Animation' href='http://scct.southerncrossinc.com/index.php?r=equipment%2Findex'>Equipment Manager</a></li>"
+            + "<li><a data-description='Image Animation' href='http://scct.southerncrossinc.com/index.php?r=time-card%2Findex'>Timecards</a></li>"
+            + "<li><a data-description='Instrument Repair' href='http://scct.southerncrossinc.com/index.php?r=mileage-card%2Findex'>Mileagecards</a></li>"
         + "</ul></li>");
     
     // var nav5 = $("<li><a href='equipmentrepairs.html'>equipment repairs</a></li>");
