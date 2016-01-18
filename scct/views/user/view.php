9c0e48f005bb292049c95aff408b490709f47f03
Michael Davis <mdavis@speardom.com>
2016-01-18 12:09:56 -0500

    Removed UserLoginID from form and view

--- scct/views/user/view.php
+++ scct/views/user/view.php
@@ -33,7 +33,6 @@ $this->params['breadcrumbs'][] = $this->title;
             'UserName',
             'UserFirstName',
             'UserLastName',
-            'UserLoginID',
             'UserEmployeeType',
             'UserPhone',
             'UserCompanyName',
