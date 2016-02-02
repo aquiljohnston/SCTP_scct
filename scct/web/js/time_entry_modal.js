2ac8c1c7af265a049f7851de3fcd0f45a0a3b976
unknown <tzhang@8l9z362.speardom.com>
2016-02-02 11:26:22 -0500

    updated timecard view and modal js files

--- scct/web/js/time_entry_modal.js
+++ scct/web/js/time_entry_modal.js
@@ -1,8 +1,50 @@
 $(function(){
-	$('#modalButton').click(function(){
+	$('#modalButtonSunday').click(function(){
 	// get the click of the create button
-		$('#modal').modal('show')
-			.find('#modalContent')
+		$('#modalSunday').modal('show')
+			.find('#modalContentSunday')
+			.load($(this).attr('value'));
+	});
+	
+	$('#modalButtonMonday').click(function(){
+	// get the click of the create button
+		$('#modalMonday').modal('show')
+			.find('#modalContentMonday')
+			.load($(this).attr('value'));
+	})
+	
+	$('#modalButtonTuesday').click(function(){
+	// get the click of the create button
+		$('#modalTuesday').modal('show')
+			.find('#modalContentTuesday')
+			.load($(this).attr('value'));
+	})
+	
+	$('#modalButtonWednesday').click(function(){
+	// get the click of the create button
+		$('#modalWednesday').modal('show')
+			.find('#modalContentWednesday')
+			.load($(this).attr('value'));
+	})
+	
+	$('#modalButtonThursday').click(function(){
+	// get the click of the create button
+		$('#modalThursday').modal('show')
+			.find('#modalContentThursday')
+			.load($(this).attr('value'));
+	})
+	
+	$('#modalButtonFriday').click(function(){
+	// get the click of the create button
+		$('#modalFriday').modal('show')
+			.find('#modalContentFriday')
+			.load($(this).attr('value'));
+	})
+	
+	$('#modalButtonSaturday').click(function(){
+	// get the click of the create button
+		$('#modalSaturday').modal('show')
+			.find('#modalContentSaturday')
 			.load($(this).attr('value'));
 	})
 });
