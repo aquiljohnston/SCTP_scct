0d37a6f37606becd56766f876e64d1c813dab6fb
unknown <tzhang@8l9z362.speardom.com>
2016-02-22 11:21:26 -0500

    clean up index.php for mileage card

--- scct/views/mileage-card/index.php
+++ scct/views/mileage-card/index.php
@@ -30,52 +30,25 @@ $this->params['breadcrumbs'][] = $this->title;
 			'MileageEndDate',
 			'MileageCardBusinessMiles',
 			'MileageCardApprove',
-            /*'MileageCardEmpID',
-            'MileageCardTechID',
-            'MileageCardProjectID',
-            'MileageCardType',*/
-
-            //['class' => 'yii\grid\ActionColumn'],
 			
 			['class' => 'yii\grid\ActionColumn',
-                             /* 'buttons'=>[
-                              'View' => function ($url, $model) { 
-                                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
-                                        'title' => Yii::t('yii', 'View'),
-                                ]);                                
-            
-                              }
-                          ], */
-							  'urlCreator' => function ($action, $model, $key, $index) {
-								  //var_dump($model["UserID"]);
-											if ($action === 'view') {
-											$url ='index.php?r=mileage-card%2Fview&id='.$model["MileageCardID"];
-											return $url;
-											}
-											if ($action === 'update') {
-											$url ='index.php?r=mileage-card%2Fupdate&id='.$model["MileageCardID"];
-											return $url;
-											}
-											if ($action === 'delete') {
-											$url ='index.php?r=mileage-card%2Fupdate&id='.$model["MileageCardID"];
-											return $url;
-											}
-										},
-										'buttons' => [
-											'delete' => function ($url, $model, $key) {
-												$url ='/index.php?r=mileage-card%2Fdelete&id='.$model["MileageCardID"];       
-													$options = [
-													'title' => Yii::t('yii', 'Delete'),
-													'aria-label' => Yii::t('yii', 'Delete'),
-													'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-													'data-method' => 'Delete',
-													'data-pjax' => '0',
-													];
-													return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, $options);
-											},
-										]											
-                            ],
+			
+				'urlCreator' => function ($action, $model, $key, $index) {
+						if ($action === 'view') {
+						$url ='index.php?r=mileage-card%2Fview&id='.$model["MileageCardID"];
+						return $url;
+						}
+						if ($action === 'update') {
+						$url ='index.php?r=mileage-card%2Fupdate&id='.$model["MileageCardID"];
+						return $url;
+						}
+						if ($action === 'delete') {
+						$url ='index.php?r=mileage-card%2Fupdate&id='.$model["MileageCardID"];
+						return $url;
+						}
+					},											
 			],
+		],
     ]); ?>
 
 </div>
