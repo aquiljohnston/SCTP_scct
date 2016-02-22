94fea5a317efdd37218139e3dd6f43a9c4ef1147
unknown <tzhang@8l9z362.speardom.com>
2016-02-22 11:06:18 -0500

    update mileage card column name

--- scct/views/mileage-card/index.php
+++ scct/views/mileage-card/index.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
 			'MileageStartDate',
 			'MileageEndDate',
 			'MileageCardBusinessMiles',
-			'MileageCardApproved',
+			'MileageCardApprove',
             /*'MileageCardEmpID',
             'MileageCardTechID',
             'MileageCardProjectID',
