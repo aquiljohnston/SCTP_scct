257a6da6432b95a3cf1ad51791e6b0283af4016f
unknown <tzhang@8l9z362.speardom.com>
2016-03-16 09:50:23 -0400

    Fixed timeentry modal view bug and added timeEntry comment to the modal view, deleted alert windows on timecard page

--- scct/web/js/approve_multiple_timecard.js
+++ scct/web/js/approve_multiple_timecard.js
@@ -1,30 +1,21 @@
 // BULK DELETE
 $('#multiple_approve_btn_id').click(function(){
-//$('#multiple_approve_btn_id').on('click',function() {
   var model = $(this).attr('model');
-  var pks = $('#w1').yiiGridView('getSelectedRows');//.attr("timecardid");
+  var pks = $('#w1').yiiGridView('getSelectedRows');
   var TimeCardIDArray = [];
   
   //alert("key is :　"+pks);
 
   if (!pks || 0 !== pks.length) {
-	  //yii.confirm = function(message, ok, cancel) {
-		  //bootbox.confirm(message, function(result) {
-			  //if (result) {
-				  $.ajax({
-					 type: 'POST',
-					 url: 'index.php?r=time-card/approve-multiple',//-multiple',//'.\yii\helpers\Url::to(['/controller/action']).'
-					 data: {timecardid: pks},
-					 success: function(data) {
-						//alert("Successfully Approved");
-						  $.pjax.reload({container:'#w0'});
-					 }
-				  });
-			  //} else { !cancel || cancel(); }
-		  //});
-	  //}
+	  $.ajax({
+		 type: 'POST',
+		 url: 'index.php?r=time-card/approve-multiple',
+		 data: {timecardid: pks},
+		 success: function(data) {
+			  $.pjax.reload({container:'#w0'});
+		 }
+	  });
   } else {
-	  alert("No TimeCard Selected!");
 	  return false;
   }
 });
--- scct/web/js/time_entry_modal.js
+++ scct/web/js/time_entry_modal.js
@@ -1,50 +1,86 @@
 $(function(){
+	
+	// Modal view for Sunday
 	$('#modalButtonSunday').click(function(){
 	// get the click of the create button
 		$('#modalSunday').modal('show')
 			.find('#modalContentSunday')
 			.load($(this).attr('value'));
+	});	
+	$('#modalSunday').on('hidden.bs.modal', function (e) {
+	  // reload page when modal closed
+	  location.reload(true);
 	});
 	
+	// Modal view for Monday
 	$('#modalButtonMonday').click(function(){
 	// get the click of the create button
 		$('#modalMonday').modal('show')
 			.find('#modalContentMonday')
 			.load($(this).attr('value'));
-	})
-	
+	});		
+	$('#modalMonday').on('hidden.bs.modal', function (e) {
+	  // reload page when modal closed
+	  location.reload(true);
+	});
+
+	// Modal view for Tuesday
 	$('#modalButtonTuesday').click(function(){
 	// get the click of the create button
 		$('#modalTuesday').modal('show')
 			.find('#modalContentTuesday')
 			.load($(this).attr('value'));
-	})
+	});	
+	$('#modalTuesday').on('hidden.bs.modal', function (e) {
+	  // reload page when modal closed
+	  location.reload(true);
+	});	
 	
+	// Modal view for Wednesday
 	$('#modalButtonWednesday').click(function(){
 	// get the click of the create button
 		$('#modalWednesday').modal('show')
 			.find('#modalContentWednesday')
 			.load($(this).attr('value'));
-	})
+	});	
+	$('#modalWednesday').on('hidden.bs.modal', function (e) {
+	  // reload page when modal closed
+	  location.reload(true);
+	});
 	
+	// Modal view for Thursday
 	$('#modalButtonThursday').click(function(){
 	// get the click of the create button
 		$('#modalThursday').modal('show')
 			.find('#modalContentThursday')
 			.load($(this).attr('value'));
-	})
+	})	
+	$('#modalThursday').on('hidden.bs.modal', function (e) {
+	  // reload page when modal closed
+	  location.reload(true);
+	});	
 	
+	// Modal view for Friday
 	$('#modalButtonFriday').click(function(){
 	// get the click of the create button
 		$('#modalFriday').modal('show')
 			.find('#modalContentFriday')
 			.load($(this).attr('value'));
-	})
+	});	
+	$('#modalFriday').on('hidden.bs.modal', function (e) {
+	  // reload page when modal closed
+	  location.reload(true);
+	});	
 	
+	// Modal view for Saturday
 	$('#modalButtonSaturday').click(function(){
 	// get the click of the create button
 		$('#modalSaturday').modal('show')
 			.find('#modalContentSaturday')
 			.load($(this).attr('value'));
-	})
+	});	
+	$('#modalSaturday').on('hidden.bs.modal', function (e) {
+	  // reload page when modal closed
+	  location.reload(true);
+	});
 });
