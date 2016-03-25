d839b5860a90d768d174e08fdcabb1c7d4654523
unknown <tzhang@8l9z362.speardom.com>
2016-03-25 11:42:15 -0400

    added single accept equipment and constrains

--- scct/views/equipment/view.php
+++ scct/views/equipment/view.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
@@ -20,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
 		
 		<?php 
 			// get approve equipment action
-			$approveUrl = "";//urldecode(Url::to(['equipment/approve', 'id' => $model["equipmentID"]]));
+			$approveUrl = urldecode(Url::to(['equipment/approve', 'id' => $model["EquipmentID"]]));
 		?>
 		<?php if($model['EquipmentAcceptedFlag']=='Yes'){ ?>
 			<?= Html::a('Accept Equipment', $approveUrl, [
