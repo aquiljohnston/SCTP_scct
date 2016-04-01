98c10cf72b70f2a503e3f11c7d0b89e7d7e3a035
unknown <tzhang@8l9z362.speardom.com>
2016-04-01 15:26:05 -0400

    added dispatch module

---
+++ scct/modules/dispatch/dispatchModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\dispatch;
+
+/**
+ * dispatch module definition class
+ */
+class dispatchModule extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\dispatch\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
