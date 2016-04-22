0036bef647dc341ea98eee3e1ffa595519596e03
Michael Davis <mdavis@speardom.com>
2016-04-22 11:17:41 -0400

    Fix for rbac assignments

--- scct/rbac/assignments.php
+++ scct/rbac/assignments.php
@@ -39,4 +39,77 @@ return [
     226 => [
         'Engineer',
     ],
+    230 => [
+        'Admin',
+        'ProjectManager',
+    ],
+    231 => [
+        'Admin',
+    ],
+    233 => [
+        'Admin',
+    ],
+    234 => [
+        'Admin',
+    ],
+    322 => [
+        'ProjectManager',
+    ],
+    331 => [
+        'Admin',
+    ],
+    345 => [
+        'Admin',
+    ],
+    352 => [
+        'ProjectManager',
+    ],
+    353 => [
+        'Supervisor',
+    ],
+    354 => [
+        'Engineer',
+    ],
+    355 => [
+        'Technician',
+    ],
+    357 => [
+        'Technician',
+    ],
+    358 => [
+        'Admin',
+    ],
+    359 => [
+        'Admin',
+    ],
+    360 => [
+        'Admin',
+    ],
+    361 => [
+        'Admin',
+    ],
+    362 => [
+        'Admin',
+    ],
+    363 => [
+        'Technician',
+    ],
+    368 => [
+        'Admin',
+    ],
+    369 => [
+        'Technician',
+    ],
+    370 => [
+        'Admin',
+    ],
+    371 => [
+        'Admin',
+    ],
+    373 => [
+        'Admin',
+    ],
+    375 => [
+        'Admin',
+    ],
 ];
