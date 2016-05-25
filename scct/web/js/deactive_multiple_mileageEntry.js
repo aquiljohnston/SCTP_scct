ff52206c93a008432d7c499095f85c841264a6ad
unknown <tzhang@8l9z362.speardom.com>
2016-05-25 14:55:55 -0400

    added constrain, which is user won't be able to deactivate Time/MileageEntry once Time/MileageCard has been approved.

--- scct/web/js/deactive_multiple_mileageEntry.js
+++ scct/web/js/deactive_multiple_mileageEntry.js
@@ -3,24 +3,22 @@ $(function() {
 	var checkDeActiveArray = [];
 	var counter = 0;
 	var flag = "Yes";
+	var approved = "No";
 	var totalworkhours = -1;
 	$('#deactive_mileageEntry_btn_id').prop('disabled', true); //TO DISABLED
 	$(".grid-view input[type=checkbox]").click(function(){
 
-		var checkDeActive = $(this).attr("activeStatus");
-		//checkDeactiveArray[counter++] = checkApproved;
-		//var model = $(this).attr('model');
 		var pks = $('.grid-view').yiiGridView('getSelectedRows');
+		
+		// get approved value for this mileagecard
+			approved = $(".mileagecard-view").attr("approved");
 
 		for(var i=0; i < pks.length; i++){
 			// get activeStatus value for this timecard
 			flag = $(".grid-view input[mileageentryid="+pks[i]+"]").attr("activeStatus");
 			
-			// get totalworkhours for this timecard
-			totalworkhours = $(".grid-view input[mileageentryid="+pks[i]+"]").attr("totalworkhours");
-			
 			//alert("loop flag is "+flag+" i is : "+i);
-			if(flag == "Inactive" || totalworkhours == .0 || flag == "Inactive"){
+			if(flag == "Inactive" || totalworkhours == .0){
 				flag = "Inactive";
 				break;
 			}else{
@@ -28,7 +26,8 @@ $(function() {
 			}
 		}
 		
-		if (!pks || pks.length != 0 && flag != "Inactive"){
+		if (!pks && pks.length != 0 && flag != "Inactive" && approved != "Yes"){
+				alert("approved if "+approved);
 				$('#deactive_mileageEntry_btn_id').prop('disabled', false); //TO ENABLE
 				
 				// triggered when checkbox selected
--- scct/web/js/deactive_multiple_timeEntry.js
+++ scct/web/js/deactive_multiple_timeEntry.js
@@ -3,13 +3,14 @@ $(function() {
 	var checkDeActiveArray = [];
 	var counter = 0;
 	var flag = "Yes";
+	var approved = "No"
 	var totalworkhours = -1;
 	$('#deactive_timeEntry_btn_id').prop('disabled', true); //TO DISABLED
 	$(".grid-view input[type=checkbox]").click(function(){
-
-		var checkDeActive = $(this).attr("activeStatus");
-		//checkDeactiveArray[counter++] = checkApproved;
-		//var model = $(this).attr('model');
+		
+		// get approved value for this timecard
+			approved = $(".timecard-view").attr("approved");
+			
 		var pks = $('.grid-view').yiiGridView('getSelectedRows');
 
 		for(var i=0; i < pks.length; i++){
@@ -20,7 +21,7 @@ $(function() {
 			totalworkhours = $(".grid-view input[timeentryid="+pks[i]+"]").attr("totalworkhours");
 			
 			//alert("loop flag is "+flag+" i is : "+i);
-			if(flag == "Inactive" || totalworkhours == .0 || flag == "Inactive"){
+			if(flag == "Inactive" || totalworkhours == .0){
 				flag = "Inactive";
 				break;
 			}else{
@@ -28,7 +29,7 @@ $(function() {
 			}
 		}
 		
-		if (!pks || pks.length != 0 && flag != "Inactive"){
+		if (!pks || pks.length != 0 && flag != "Inactive" && approved != "Yes"){
 				$('#deactive_timeEntry_btn_id').prop('disabled', false); //TO ENABLE
 				
 				// triggered when checkbox selected
