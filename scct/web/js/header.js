12be5c6d2203907e13cf170f13761fb590d753c8
unknown <tzhang@8l9z362.speardom.com>
2016-05-27 09:26:14 -0400

    changed dropdown content and added separator line to dropdown list

--- scct/web/js/header.js
+++ scct/web/js/header.js
@@ -141,7 +141,7 @@ $(document).ready(function(){
 				//alert("success to get projects! "+data.projects);
 				var Data = $.parseJSON(data.projects);
 				$('#projects_dropdown').empty();
-				$('#projects_dropdown').append('<li><a data-description="All Projects" href="index.php?r=project-landing%2Findex">All Projects</a></li>');
+				$('#projects_dropdown').append('<li><a data-description="All Projects" href="index.php?r=project-landing%2Findex">My Projects</a></li><hr id="seperator_line">');
 				
 				$.each(Data, function(i, item){
 					//alert("project name are "+Data[i].ProjectName);
