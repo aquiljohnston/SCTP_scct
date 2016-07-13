af09b32ae757d0e4579c39d86002d732b9e47b7a
Joshua Patton <jpatton@speardom.com>
2016-07-13 13:18:10 -0400

    Modified nav menu to no longer use front end role based authentication

--- scct/views/layouts/main.php
+++ scct/views/layouts/main.php
@@ -53,21 +53,11 @@ AppAsset::register($this);
 			<?php }?>
 			</div>
 			<div class="container">
-			<?php if (Yii::$app->user->isGuest){?>
-					<div class="loginMenu sc_megamenu"></div>
-			<?php }else{?>
-			<?php $userRole = Yii::$app->authManager->getRolesByUser(Yii::$app->session['userID']);?>
-			<?php $role = current($userRole);?>
-			<?php Yii::Trace("Session userID is : ".$role->name);?>
-			<?php if(($role->name) == "Admin") {?>
-					<div class="adminMenu sc_megamenu" id="<?php echo Yii::$app->session['userID']; ?>"></div>
-			<?php } else if (($role->name) == "Supervisor" || ($role->name) == "Engineer" ||
-						($role->name) == "ProjectManager") { ?>
-					<div class="middlePrivilegeMenu sc_megamenu" id="<?php echo Yii::$app->session['userID']; ?>"></div>
-			<?php }else{ ?>
-					<div class="menu sc_megamenu" id="<?php echo Yii::$app->session['userID']; ?>"></div>
-				<?php }?>
-			<?php }?>
+			<?php if (Yii::$app->user->isGuest): ?>
+				<div class="loginMenu sc_megamenu"></div>
+			<?php else:?>
+				<div class="menu sc_megamenu" id="<?php echo Yii::$app->session['userID']; ?>"></div>
+			<?php endif; ?>
 			</div>
 		</div>
 		<!--//End-header-->
