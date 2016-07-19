8c816aaa1786ea2d76a851ac25a465172502b146
Michael Davis <mdavis@speardom.com>
2016-07-19 13:34:48 -0400

    Added project url prefix field to front end

--- scct/views/project/view.php
+++ scct/views/project/view.php
@@ -37,6 +37,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'ProjectNotes',
             'ProjectType',
             'ProjectStatus',
+            'ProjectUrlPrefix',
             'ProjectClientID',
 			'ProjectState',
             'ProjectStartDate',
