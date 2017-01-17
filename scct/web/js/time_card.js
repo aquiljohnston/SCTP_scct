972af5a5179c4cbb09e246a67a1c9da008e612ee
tzhang <tzhang@8l9z362.speardom.com>
2017-01-17 13:30:40 -0500

    added time_card.js file

---
+++ scct/web/js/time_card.js
@@ -0,0 +1,43 @@
+$(function(){
+    var jqTCGridViewContainer = $('#timeCardGridViewContainer');
+    var jqTCDropDowns = $('#timeCardDropdownContainer');
+    var jqWeekSelection = jqTCDropDowns.find('#weekSelection');
+    var jqTCPageSize = jqTCDropDowns.find('#timeCardPageSize');
+
+
+    jqWeekSelection.on('change', function (event) {
+        event.preventDefault();
+        reloadGridView();
+        return false;
+    });
+
+    jqTCPageSize.on('change', function (event) {
+        reloadGridView();
+        event.preventDefault();
+        return false;
+    });
+
+    $(document).off('click', "#TCPagination ul li a").on('click', "#TCPagination ul li a", function () {
+        $('#loading').show();
+        $('#timeCardGridview').on('pjax:success', function () {
+            $('#loading').hide();
+        });
+    });
+    
+    function reloadGridView() {
+        var form = jqTCDropDowns.find("#TimeCardForm");
+        if (form.find(".has-error").length){
+            return false;
+        }
+        $('#loading').show();
+        $.pjax.reload({
+            type: 'POST',
+            url: form.attr("action"),
+            container: '#timeCardGridview', // id to update content
+            data: form.serialize(),
+            timeout: 99999
+        }).done(function () {
+            $('#loading').hide();
+        });
+    }
+});
