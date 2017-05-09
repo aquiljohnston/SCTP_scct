dc2112be579ad8074c25bca2e01894b064bb19ff
Josh Patton <jpatton@southerncrossinc.com>
2017-05-09 15:13:41 -0400

    Fixed equipment approve to single confirmation box, support for page changes (no persistence though), support for multiple selections
    Fixed typo in eqiupment/index.php

--- scct/web/js/approve_equipment.js
+++ scct/web/js/approve_equipment.js
@@ -1,99 +1,100 @@
 // BULK DELETE
 $(function() {
-	var counter = 0;
-	var flag = "Yes";
-	var totalworkhours = -1;
-	$('#multiple_approve_btn_id_equipment').prop('disabled', true); //TO DISABLED
-	$('.select-on-check-all').change(function() {
-		var checkApproved = $(this).attr("accepted");
 
-		//alert("checkApproved is :　"+checkApproved);
-		var model = $(this).attr('model');
-		var pks = $('#w0').yiiGridView('getSelectedRows');
-		//alert("pks length is ：　"+pks);
-		for(var i=0; i < pks.length; i++){
-			// get approved value for this timecard
-			flag = $(".kv-row-select input[equipmentid="+pks[i]+"]").attr("accepted");
-			
-			//alert("loop flag is "+flag+" i is : "+i);
-			if(flag == "Yes" || flag == "yes"){
-				flag = "Yes";
-				break;
-			}else{
-				continue;
-			}
-		}
-		
-		if (!pks || pks.length != 0 && flag != "Yes"){
-				$('#multiple_approve_btn_id_equipment').prop('disabled', false); //TO ENABLE
-				
-				// triggered when checkbox selected
-				$('#multiple_approve_btn_id_equipment').click(function(){
-				var confirmBox = confirm('Are you sure you want to approve this item?');
-					if(confirmBox){
-					
-						$.ajax({
-							type: 'POST',
-							url: '/equipment/approve-multiple-equipment',
-							data: {equipmentid: pks},
-							success: function(data) {
-								$.pjax.reload({container:'#w0'});
-							}
-						});
-					}else{
-						e.stopImmediatePropagation();
-						e.preventDefault();
-					}   
-				});
-			}else {
-					$('#multiple_approve_btn_id_equipment').prop('disabled', true);
-				}
-	});
-	
-	$("#GridViewForEquipment .kv-row-select input[type=checkbox]").click(function(){
-		var checkApproved = $(this).attr("accepted");
+    //$("#equipmentPagination a").click(applyEquipmentCheckboxesOnClickListeners); // Send function as argument
 
-		//alert("checkApproved is :　"+checkApproved);
-		var model = $(this).attr('model');
-		var pks = $('#GridViewForEquipment').yiiGridView('getSelectedRows');
-		//alert("pks length is ：　"+pks);
-		for(var i=0; i < pks.length; i++){
-			// get approved value for this timecard
-			flag = $(".kv-row-select input[equipmentid="+pks[i]+"]").attr("accepted");
-			
-			//alert("loop flag is "+flag+" i is : "+i);
-			if(flag == "Yes" || flag == "yes"){
-				flag = "Yes";
-				break;
-			}else{
-				continue;
-			}
-		}
-		
-		if (!pks || pks.length != 0 && flag != "Yes"){
-				$('#multiple_approve_btn_id_equipment').prop('disabled', false); //TO ENABLE
-				
-				// triggered when checkbox selected
-				$('#multiple_approve_btn_id_equipment').click(function(e){
-				var confirmBox = confirm('Are you sure you want to approve this item?');
-					if(confirmBox){
+    $('#equipmentGridview').on('pjax:success', function(event, data, status, xhr, options) {
+        applyEquipmentCheckboxesOnClickListeners();
+    });
 
-					  $.ajax({
-						 type: 'POST',
-						 url: '/equipment/approve-multiple-equipment',
-						 data: {equipmentid: pks},
-						 success: function(data) {
-							  $.pjax.reload({container:'#w0'});
-						 }
-					  });
-					}else{
-						e.stopImmediatePropagation();
-						e.preventDefault();
-					} 
-				});
-			}else {
-					$('#multiple_approve_btn_id_equipment').prop('disabled', true);
-				}
-	}); 
+	applyEquipmentApproveOnClickListeners();
+	applyEquipmentCheckboxesOnClickListeners()
  });
+
+function applyEquipmentCheckboxesOnClickListeners() { var counter = 0;
+    var flag = "Yes";
+    var totalworkhours = -1;
+    $('#multiple_approve_btn_id_equipment').prop('disabled', true); //TO DISABLED
+    $('.select-on-check-all').change(function () {
+        var checkApproved = $(this).attr("accepted");
+
+        //alert("checkApproved is :　"+checkApproved);
+        var model = $(this).attr('model');
+        var pks = $('#w0').yiiGridView('getSelectedRows');
+        //alert("pks length is ：　"+pks);
+        for (var i = 0; i < pks.length; i++) {
+            // get approved value for this timecard
+            flag = $(".kv-row-select input[equipmentid=" + pks[i] + "]").attr("accepted");
+
+            //alert("loop flag is "+flag+" i is : "+i);
+            if (flag == "Yes" || flag == "yes") {
+                flag = "Yes";
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        if (!pks || pks.length != 0 && flag != "Yes") {
+            $('#multiple_approve_btn_id_equipment').prop('disabled', false); //TO ENABLE
+        } else {
+            $('#multiple_approve_btn_id_equipment').prop('disabled', true);
+        }
+    });
+
+    $("#GridViewForEquipment .kv-row-select input[type=checkbox]").click(function () {
+        var checkApproved = $(this).attr("accepted");
+
+        //alert("checkApproved is :　"+checkApproved);
+        var model = $(this).attr('model');
+        var pks = $('#GridViewForEquipment').yiiGridView('getSelectedRows');
+        //alert("pks length is ：　"+pks);
+        for (var i = 0; i < pks.length; i++) {
+            // get approved value for this timecard
+            flag = $(".kv-row-select input[equipmentid=" + pks[i] + "]").attr("accepted");
+
+            //alert("loop flag is "+flag+" i is : "+i);
+            if (flag == "Yes" || flag == "yes") {
+                flag = "Yes";
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        if (!pks || pks.length != 0 && flag != "Yes") {
+            $('#multiple_approve_btn_id_equipment').prop('disabled', false); //TO ENABLE
+        } else {
+            $('#multiple_approve_btn_id_equipment').prop('disabled', true);
+        }
+    });
+
+
+}
+function applyEquipmentApproveOnClickListeners() {
+    $('#multiple_approve_btn_id_equipment').click(function () {
+        var primaryKeys = $('#GridViewForEquipment').yiiGridView('getSelectedRows');
+        var quantifier = "";
+        if(primaryKeys.length <= 1 ) { // We don't expect 0 or negative but we need to handle it
+            quantifier = "this item?";
+        } else {
+            quantifier = "these items?"
+        }
+        var confirmBox = confirm('Are you sure you want to approve ' + quantifier);
+        if (confirmBox) {
+            $.ajax({
+                type: 'POST',
+                url: '/equipment/approve-multiple-equipment',
+                data: {equipmentid: primaryKeys},
+                success: function (data) {
+                    $.pjax.reload({container: '#w0'});
+                }
+            });
+        } else {
+            e.stopImmediatePropagation();
+            e.preventDefault();
+        }
+    });
+}
+
 	 
