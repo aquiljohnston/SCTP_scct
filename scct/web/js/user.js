6323c2b8fdd1a10bc0f9e25885d766962a006067
Josh Patton <jpatton@southerncrossinc.com>
2017-05-17 14:38:43 -0400

    Fixed User Management page
    Used PGE-web code and strategy

--- scct/web/js/user.js
+++ scct/web/js/user.js
@@ -39,4 +39,30 @@ $(function () {
             $('#loading').hide();
         });
     }
+
+    userManagementPaginationListener();
 });
+function userManagementPaginationListener() {
+    $(document).off('click', '#UserPagination .pagination li a').on('click', '#UserPagination .pagination li a', function (event) {
+        event.preventDefault();
+        var page = $(this).data('page') + 1; // Shift by one to 1-index instead of 0-index.
+        $('#UserManagementPageNumber').val(page);
+        var form = $('#UserForm');
+        $('#loading').show();
+        $.pjax.reload({
+            container: "#userGridview",
+            timeout: 99999,
+            url: form.attr("action"),
+            type: "get",
+            data: form.serialize()
+        });
+    });
+    $('#userGridview').on('pjax:success', function (event, data, status, xhr, options) {
+        $('#loading').hide();
+    });
+    $('#userGridview').on('pjax:error', function (event, data, status, xhr, options) {
+        console.log("Error loading PJAX user management gridview");
+        //TODO: Consider resending PJAX call
+    });
+}
+
