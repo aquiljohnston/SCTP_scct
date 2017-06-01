7c402b11ff8160be2acd2980e24c96e8bb28d42c
tzhang <tzhang@8l9z362.speardom.com>
2017-06-01 10:25:06 -0400

    1. added pjax call 2. added search and drop down filters

--- scct/web/js/assigned.js
+++ scct/web/js/assigned.js
@@ -41,6 +41,9 @@ $(function () {
             reloadAssignedGridView();
         }
     });
+    $(document).off('change', '#assignPageSize').on('change', '#assignPageSize', function () {
+        reloadAssignedGridView();
+    });
     unassignCheckboxListener();
 });
 
