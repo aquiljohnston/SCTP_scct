ebc9a8a8ce77341cae40ba89e20f9c39785084f7
tzhang <tzhang@8l9z362.speardom.com>
2017-06-01 10:25:06 -0400

    1. added pjax call 2. added search and drop down filters

--- scct/web/js/dispatch.js
+++ scct/web/js/dispatch.js
@@ -1,77 +1,90 @@
-// Refer the modal in dispatch page
-$('#dispatchButton').click(function () {
-    /*var pks = $('#dispatchGV').yiiGridView('getSelectedRows');
-    var pks_surveyors = $('#surveyorsGV').yiiGridView('getSelectedRows');
-    $.ajax({
-        type: 'POST',
-        url: '/dispatch/dispatch/post',
-        data: {InspectionRequestUID: pks, UserUID: pks_surveyors},
-        beforeSend: function () {
-            $('#loading').show();
-        },
-        complete: function () {
-            window.location = window.location; // ?
-            $('#dispatchActiveForm').submit().done(function () {
-                $('#loading').hide();
-            });
-        }
-    });*/
-    var MapPlatArr = [];
-    var IRUIDArr = [];
-    console.log("clicked");
-    $('#addSurveyorModal').modal('show')
-        .find('#modalAddSurveyor')
-        .load('/dispatch/add-surveyor-modal/add-surveyor-modal', {
-            "mapplat[]": [MapPlatArr],
-            "IRUID[]": [IRUIDArr]
-        });
-});
-
-// Refer the modal in assigned page
-$('#addSurveyor').click(function () {
-    /*var pks = $('#dispatchGV').yiiGridView('getSelectedRows');
-     var pks_surveyors = $('#surveyorsGV').yiiGridView('getSelectedRows');
-     $.ajax({
-     type: 'POST',
-     url: '/dispatch/dispatch/post',
-     data: {InspectionRequestUID: pks, UserUID: pks_surveyors},
-     beforeSend: function () {
-     $('#loading').show();
-     },
-     complete: function () {
-     window.location = window.location; // ?
-     $('#dispatchActiveForm').submit().done(function () {
-     $('#loading').hide();
-     });
-     }
-     });*/
-    var MapPlatArr = [];
-    var IRUIDArr = [];
-    console.log("clicked");
-    $('#addSurveyorModal').modal('show')
-        .find('#modalAddSurveyor')
-        .load('/dispatch/add-surveyor-modal/add-surveyor-modal', {
-            "mapplat[]": [MapPlatArr],
-            "IRUID[]": [IRUIDArr]
-        });
-});
-
 $(function () {
     //$("#dispatchButton").prop('disabled', true);
-});
-$('.dispatch input[type=checkbox]').click(function () {
-    // This Section for Dispatch page Dispatch Function
-    var checkApproved = $(this).attr("approved");
-    var pks = $('#dispatchGV').yiiGridView('getSelectedRows');
-    var pks_surveyors = $('#surveyorsGV').yiiGridView('getSelectedRows');
-    console.log("unassign: " + pks.length);
-    console.log("surveyor: " + pks_surveyors.length);
 
-    if ((!pks || pks.length != 0) && (!pks_surveyors || pks_surveyors.length != 0)) {
-        $('#dispatchButton').prop('disabled', false); //TO ENABLE
-    } else {
-        $('#dispatchButton').prop('disabled', true);
-    }
+    // dispatch filter listener
+    $(document).off('keypress', '#dispatchFilter').on('keypress', '#dispatchFilter', function (e) {
+        if (e.keyCode === 13 || e.keyCode === 10) {
+            e.preventDefault();
+            reloadDispatchGridView();
+        }
+    });
+
+    //page size listener
+    $(document).off('change', '#dispatchPageSize').on('change', '#dispatchPageSize', function () {
+        reloadDispatchGridView();
+    });
+
+    // Refer the modal in dispatch page
+    $('#dispatchButton').click(function () {
+        /*var pks = $('#dispatchGV').yiiGridView('getSelectedRows');
+         var pks_surveyors = $('#surveyorsGV').yiiGridView('getSelectedRows');
+         $.ajax({
+         type: 'POST',
+         url: '/dispatch/dispatch/post',
+         data: {InspectionRequestUID: pks, UserUID: pks_surveyors},
+         beforeSend: function () {
+         $('#loading').show();
+         },
+         complete: function () {
+         window.location = window.location; // ?
+         $('#dispatchActiveForm').submit().done(function () {
+         $('#loading').hide();
+         });
+         }
+         });*/
+        var MapPlatArr = [];
+        var IRUIDArr = [];
+        console.log("clicked");
+        $('#addSurveyorModal').modal('show')
+            .find('#modalAddSurveyor')
+            .load('/dispatch/add-surveyor-modal/add-surveyor-modal', {
+                "mapplat[]": [MapPlatArr],
+                "IRUID[]": [IRUIDArr]
+            });
+    });
+
+    // Refer the modal in assigned page
+    $('#addSurveyor').click(function () {
+        /*var pks = $('#dispatchGV').yiiGridView('getSelectedRows');
+         var pks_surveyors = $('#surveyorsGV').yiiGridView('getSelectedRows');
+         $.ajax({
+         type: 'POST',
+         url: '/dispatch/dispatch/post',
+         data: {InspectionRequestUID: pks, UserUID: pks_surveyors},
+         beforeSend: function () {
+         $('#loading').show();
+         },
+         complete: function () {
+         window.location = window.location; // ?
+         $('#dispatchActiveForm').submit().done(function () {
+         $('#loading').hide();
+         });
+         }
+         });*/
+        var MapPlatArr = [];
+        var IRUIDArr = [];
+        console.log("clicked");
+        $('#addSurveyorModal').modal('show')
+            .find('#modalAddSurveyor')
+            .load('/dispatch/add-surveyor-modal/add-surveyor-modal', {
+                "mapplat[]": [MapPlatArr],
+                "IRUID[]": [IRUIDArr]
+            });
+    });
+    $('.dispatch input[type=checkbox]').click(function () {
+        // This Section for Dispatch page Dispatch Function
+        var checkApproved = $(this).attr("approved");
+        var pks = $('#dispatchGV').yiiGridView('getSelectedRows');
+        var pks_surveyors = $('#surveyorsGV').yiiGridView('getSelectedRows');
+        console.log("unassign: " + pks.length);
+        console.log("surveyor: " + pks_surveyors.length);
+
+        if ((!pks || pks.length != 0) && (!pks_surveyors || pks_surveyors.length != 0)) {
+            $('#dispatchButton').prop('disabled', false); //TO ENABLE
+        } else {
+            $('#dispatchButton').prop('disabled', true);
+        }
+    });
 });
 
 function dispatchButtonListener() {
@@ -119,5 +132,23 @@ function dispatchButtonListener() {
 
     // disable dispatch button again
     $('.dispatch_btn').prop('disabled', true); //TO DISABLED
+}
+
+function reloadDispatchGridView() {
+    var jqDispatchDropDowns = $('#dispatchTab');
+    var form = jqDispatchDropDowns.find("#dispatchActiveForm");
+    if (form.find(".has-error").length) {
+        return false;
+    }
+    $('#loading').show();
+    $.pjax.reload({
+        type: 'GET',
+        url: form.attr("action"),
+        container: '#dispatchUnassignedGridview', // id to update content
+        data: form.serialize(),
+        timeout: 99999
+    }).done(function () {
+        $('#loading').hide();
+    });
 }
 
