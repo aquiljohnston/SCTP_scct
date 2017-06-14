0bb1db0a2d062c591632d0bfd1d502627cc4590a
tzhang <tzhang@8l9z362.speardom.com>
2017-06-14 16:42:41 -0400

    refactored project page

--- scct/views/project/add_user.php
+++ scct/views/project/add_user.php
@@ -15,7 +15,6 @@ $this->params['breadcrumbs'][] = ['label' => $project->ProjectID, 'url' => ['vie
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="project-add-user">
-
     <h1 class="title"><?= Html::encode($this->title) ?></h1>
 
 	<p>
@@ -31,53 +30,58 @@ $this->params['breadcrumbs'][] = $this->title;
 			<?= $form->field($model, 'filter')->textInput(['value' => $projectFilterParams, 'id' => 'projectFilter'])->label('Search'); ?>
 		</label>
 		<input type="hidden" value=<?php echo $project->ProjectID;?> name="projectID" id="projectID">
-		<input id="dispatchPageNumber" type="hidden" name="dispatchPageNumber" value="1"/>
-	<div class="form-group">
-        <?= Html::submitButton( 'Submit', ['class' => 'btn btn-success','id' => 'projectAddUserSubmitBtn']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-default','id' => 'projectAddUserResetBtn']) ?>
-    </div>
-    <?php ActiveForm::end(); ?>
+	<?php ActiveForm::end(); ?>
+		<?php Pjax::begin(['id' => 'projectSortableView', 'timeout' => false]) ?>
+		<?php $form = ActiveForm::begin([
+			'type' => ActiveForm::TYPE_VERTICAL,
+			'formConfig' => ['showLabels' => false,'deviceSize' => ActiveForm::SIZE_SMALL],
+			'options' => ['id' => 'projectSortableInputForm']
+		]); ?>
+		<div class="row">
+			<div class="col-sm-6">
+				<label style="font-size:20px">Unassigned Users</label>
+				<div class="projectAddUserForms">
+					<?= $form->field($model, 'UnassignedUsers')->widget(SortableInput::classname(),[
+						//'name'=>'Unassigned Users',
+						'items' => $unassignedData,
+						'hideInput' => true,
+						'sortableOptions' => [
+							'connected'=>true,
+							'options' => ['style'=> 'min-height: 20pt']
+						],
+						'options' => [
+							'class'=>'form-control',
+							'readonly'=>true,
+						]
+					]); ?>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<label style="font-size:20px">Assigned Users</label>
+				<div class="projectAddUserForms">
+					<?= $form->field($model, 'AssignedUsers')->widget(SortableInput::classname(),[
+						'name'=>'Assigned Users',
+						'items' => $assignedData,
+						'hideInput' => true,
+						'sortableOptions' => [
+							'itemOptions'=>['class'=>'alert alert-warning'],
+							'connected'=>true,
+							'options' => ['style'=> 'min-height: 20pt']
 
-	<?php Pjax::begin(['id' => 'projectSortableView', 'timeout' => false]) ?>
-	<div class="row">
-		<div class="col-sm-6">
-			<label style="font-size:20px">Unassigned Users</label>
-			<div class="projectAddUserForms">
-				<?= $form->field($model, 'UnassignedUsers')->widget(SortableInput::classname(),[
-					//'name'=>'Unassigned Users',
-					'items' => $unassignedData,
-					'hideInput' => true,
-					'sortableOptions' => [
-						'connected'=>true,
-						'options' => ['style'=> 'min-height: 20pt']
-					],
-					'options' => [
-						'class'=>'form-control',
-						'readonly'=>true,
-					]
-				]); ?>
+						],
+						'options' => [
+							'class'=>'form-control',
+							'readonly'=>true
+						]
+					]); ?>
+				</div>
 			</div>
 		</div>
-		<div class="col-sm-6">
-			<label style="font-size:20px">Assigned Users</label>
-			<div class="projectAddUserForms">
-				<?= $form->field($model, 'AssignedUsers')->widget(SortableInput::classname(),[
-					'name'=>'Assigned Users',
-					'items' => $assignedData,
-					'hideInput' => true,
-					'sortableOptions' => [
-						'itemOptions'=>['class'=>'alert alert-warning'],
-						'connected'=>true,
-						'options' => ['style'=> 'min-height: 20pt']
-
-					],
-					'options' => [
-						'class'=>'form-control',
-						'readonly'=>true
-					]
-				]); ?>
-			</div>
-		</div>
-	</div>
+		<input type="hidden" value=<?php echo $project->ProjectID;?> name="projectID" id="projectID">
+	<?php ActiveForm::end(); ?>
 	<?php Pjax::end() ?>
+	<div class="form-group">
+		<?= Html::Button( 'Submit', ['class' => 'btn btn-success','id' => 'projectAddUserSubmitBtn']) ?>
+		<?= Html::resetButton('Reset', ['class' => 'btn btn-default','id' => 'projectAddUserResetBtn']) ?>
+	</div>
 </div>
