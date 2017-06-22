4fc4ee558014ffb49e1b1d509fdb700a86e696b7
Josh Patton <jpatton@southerncrossinc.com>
2017-06-22 16:17:11 -0400

    Added TrackerController with landing page for tracker that has a dropdown with InProgress and Completed MapGrids.

---
+++ scct/views/tracker/landing.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpatton
+ * Date: 6/21/2017
+ * Time: 4:04 PM
+ */
+use yii\bootstrap\Html;
+
+$this->title = 'Tracker History';
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div id="trackerLanding">
+    <h3 class="title"><?= Html::encode($this->title) ?></h3>
+    <label for="mapGrid">Map Grid (In Progress or Completed): </label>
+    <?php
+    echo Html::dropDownList("mapGrid", null, $dropdown, []);
+    ?>
+</div>
