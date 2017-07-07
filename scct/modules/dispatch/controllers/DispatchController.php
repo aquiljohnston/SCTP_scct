0da18b97aeb65abf71b9eaaa76461ab20ea70b19
tzhang <tzhang@8l9z362.speardom.com>
2017-07-07 18:05:10 -0400

    Revert "Light Dispatch - Lightweight page that AJAX loads the heavy dispatch page while displaying a loading indicator."
    This reverts commit e0f806fd2341f1d40444300e69eab31a92e0d045.

--- scct/modules/dispatch/controllers/DispatchController.php
+++ scct/modules/dispatch/controllers/DispatchController.php
@@ -9,11 +9,10 @@ use yii\data\ArrayDataProvider;
 use yii\web\ForbiddenHttpException;
 use yii\web\Response;
 use yii\data\Pagination;
-use yii\web\ServerErrorHttpException;
 
 class DispatchController extends \app\controllers\BaseController
 {
-    public function actionHeavyDispatch()
+    public function actionIndex()
     {
         try {
 
@@ -110,14 +109,8 @@ class DispatchController extends \app\controllers\BaseController
             throw new ForbiddenHttpException('You do not have adequate permissions to perform this action.');
         } catch (Exception $e) {
             Yii::$app->runAction('login/user-logout');
-            return "";
         }
     }
-
-    public function actionIndex() {
-        return $this->render('lightDispatch');
-    }
-
 
     /**
      * render expandable section row
