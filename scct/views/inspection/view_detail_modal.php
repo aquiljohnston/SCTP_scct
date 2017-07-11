ee952799dd67c5446ceb6285918ed1d918984b1f
tzhang <tzhang@8l9z362.speardom.com>
2017-07-11 09:32:58 -0400

    added detail modal to show event of section

---
+++ scct/views/inspection/view_detail_modal.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tzhang
+ * Date: 6/6/2017
+ * Time: 1:23 PM
+ */
+use kartik\form\ActiveForm;
+use yii\helpers\Html;
+use yii\widgets\Pjax;
+use yii\widgets\LinkPager;
+use kartik\grid\GridView;
+
+?>
+<div id="viewAssetModalContainer">
+    <div id="assetInspectionContainer">
+        <?php yii\widgets\Pjax::begin(['id' => 'assetInspectionForm']) ?>
+        <?php $form = ActiveForm::begin([
+            'type' => ActiveForm::TYPE_VERTICAL,
+        ]); ?>
+        <div class="viewAssetsSearchcontainer dropdowntitle">
+            <?= $form->field($model, 'modalSearch')->textInput(['value' => $searchFilterVal, 'id' => 'viewAssetsSearchInspection', 'placeholder'=>'Search'])->label(''); ?>
+        </div>
+        <?php echo Html::img('@web/logo/filter_clear_black.png', ['id' => 'assetsModalCleanFilterButtonInspection']) ?>
+        <input id="searchFilterVal" type="hidden" name="searchFilterVal" value=<?php echo $searchFilterVal; ?> />
+        <input id="mapGridSelected" type="hidden" name="mapGridSelected" value=<?php echo $mapGridSelected; ?> />
+        <input id="sectionNumberSelected" type="hidden" name="sectionNumberSelected" value=<?php echo $sectionNumberSelected; ?> />
+        <?php ActiveForm::end(); ?>
+        <?php yii\widgets\Pjax::end() ?>
+    </div>
+</div>
+<div id="assetsTable">
+    <?php Pjax::begin([
+        'id' => 'assetTablePjax',
+        'timeout' => 10000,
+        'enablePushState' => false]) ?>
+
+    <?= GridView::widget([
+        'id' => 'assetGV',
+        'dataProvider' => $assetDataProvider,
+        'export' => false,
+        'pjax' => true,
+        'summary' => '',
+        'columns' => [
+            [
+                'class' => 'kartik\grid\CheckboxColumn',
+                'header' => 'Select',
+                'contentOptions' => ['class' => 'AddSurveyor'],
+                'checkboxOptions' => function ($model, $key, $index, $column) {
+                    if (!empty($model)) {
+                        return ['WorkOrderID' => $model["WorkOrderID"]];
+                    }
+                },
+            ],
+            [
+                'label' => 'Address',
+                'attribute' => 'HouseNumber',
+                'headerOptions' => ['class' => 'text-center'],
+                'contentOptions' => ['class' => 'text-center'],
+                'format' => 'raw',
+                'value' => function($model){
+                    return $model['HouseNumber'] . " " . $model['Street']. " " .$model['AptSuite']. "<br/>" . $model['City'] . " , " . $model['State'] . " " . $model['Zip'];
+                }
+            ],
+            [
+                'label' => 'Meter Number',
+                'attribute' => 'MeterNumber',
+                'headerOptions' => ['class' => 'text-center'],
+                'contentOptions' => ['class' => 'text-center'],
+            ],
+            [
+                'label' => 'Account Phone#',
+                'attribute' => 'AccountTelephoneNumber',
+                'headerOptions' => ['class' => 'text-center'],
+                'contentOptions' => ['class' => 'text-center'],
+            ],
+            [
+                'label' => 'Attempt Count',
+                'attribute' => 'InspectionAttemptCounter',
+                'headerOptions' => ['class' => 'text-center'],
+                'contentOptions' => ['class' => 'text-center'],
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end() ?>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#viewAssetsSearchInspection').keypress(function (event) {
+            var key = event.which;
+            if (key == 13) {
+                var searchFilterVal = $('#viewAssetsSearchInspection').val();
+                console.log("about to call");
+                console.log("searchFilterVal: " + searchFilterVal);
+                if (event.keyCode == 13) {
+                    event.preventDefault();
+                    reloadViewAssetsModalInspection();
+                }
+            }
+        });
+
+        $('#assetsModalCleanFilterButtonInspection').on('click', function () {
+            $('#viewAssetsSearchInspection').val("");
+            reloadViewAssetsModalInspection();
+        })
+    });
+
+    function reloadViewAssetsModalInspection() {
+        var form = $('#viewAssetsFormInspection');
+        var searchFilterVal = $('#viewAssetsSearchInspection').val() == "/" ? "" : $('#viewAssetsSearchInspection').val();
+        var mapGridSelected = $('#mapGridSelected').val() == "/" ? "" : $('#mapGridSelected').val();
+        var sectionNumberSelected = $('#sectionNumberSelected').val() == "/" ? "" : $('#sectionNumberSelected').val();
+        console.log("searchFilterVal: "+searchFilterVal+" mapGridSelected: "+mapGridSelected+" sectionNumberSelected: "+sectionNumberSelected);
+        $.pjax.reload({
+            type: 'GET',
+            url: '/inspection/view-asset',
+            container: '#assetTablePjax', // id to update content
+            data: {searchFilterVal: searchFilterVal, mapGridSelected: mapGridSelected, sectionNumberSelected: sectionNumberSelected},
+            timeout: 99999
+        }).done(function () {
+            $("body").css("cursor", "default");
+        });
+    }
+</script>
+
