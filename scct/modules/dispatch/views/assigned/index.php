2ebbfe0e26bef56f44367751164c049949f3bd33
Michael Davis <mdavis@southerncrossinc.com>
2017-08-02 11:08:43 -0400

    Removed link to view map on assigned screen.

--- scct/modules/dispatch/views/assigned/index.php
+++ scct/modules/dispatch/views/assigned/index.php
@@ -147,13 +147,13 @@ $pageSize = ["50" => "50", "100" => "100", "200" => "200"];
                         'headerOptions' => ['class' => 'text-center', 'style' => 'width: 5%'],
                         'contentOptions' => ['class' => 'text-center', 'style' => 'width: 5%'],
                         'format' => 'raw',
-                        'value' => function ($model, $key, $index) {
-                            return Html::a(
-                                $model['Percent Completed']."<br> View Map",
-                                ['/../tracker/view-map?mapgrid='.$model['MapGrid']],
-                                ['target'=>'_blank', 'data-pjax'=>"0"]
-                            );
-                        }
+                        // 'value' => function ($model, $key, $index) {
+                            // return Html::a(
+                                // $model['Percent Completed']."<br> View Map",
+                                // ['/../tracker/view-map?mapgrid='.$model['MapGrid']],
+                                // ['target'=>'_blank', 'data-pjax'=>"0"]
+                            // );
+                        // }
                     ],
 					[
                         'label' => 'Remaining/Total',
