e22615fd3c477d3496c9df84e45f97b2c529ce12
tzhang <tzhang@8l9z362.speardom.com>
2017-10-04 14:39:13 -0400

    completed CGE expand table

--- scct/modules/dispatch/controllers/CgeController.php
+++ scct/modules/dispatch/controllers/CgeController.php
@@ -100,4 +100,129 @@ class CgeController extends \app\controllers\BaseController
             Yii::$app->runAction('login/user-logout');
         }
     }
+
+    /**
+     * render expandable section row
+     * @return string|Response
+     */
+    public function actionViewSection()
+    {
+        // Verify logged in
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/login']);
+        }
+
+        // get the key to generate section table
+        if (isset($_POST['expandRowKey']))
+            $mapGridSelected = $_POST['expandRowKey'];
+        else
+            $mapGridSelected = "";
+        //todo: get-history&workOrderID=x
+        $getUrl = 'cge%2Fget-by-map&' . http_build_query([
+                'mapGrid' => $mapGridSelected,
+                /*'filter' => "",
+                'listPerPage' => 100,
+                'page' => 1*/
+            ]);
+        $getSectionDataResponseResponse = json_decode(Parent::executeGetRequest($getUrl, Constants::API_VERSION_2), true); //indirect RBAC
+        Yii::trace("GET SECTION: ".json_encode($getSectionDataResponseResponse));
+        $sectionData = $getSectionDataResponseResponse['cges'];
+
+        //set paging on assigned table
+        //$pages = new Pagination($getSectionDataResponseResponse['pages']);
+
+        $sectionDataProvider = new ArrayDataProvider
+        ([
+            'allModels' => $sectionData,
+            'pagination' => false,
+        ]);
+
+        //$sectionDataProvider->key = 'SectionNumber';
+
+        if (Yii::$app->request->isAjax) {
+            return $this->renderAjax('_section-expand', [
+                'sectionDataProvider' => $sectionDataProvider,
+                //'pages' => $pages,
+            ]);
+        } else {
+            return $this->render('_section-expand', [
+                'sectionDataProvider' => $sectionDataProvider,
+                //'pages' => $pages,
+            ]);
+        }
+    }
+
+    /**
+     * render asset modal
+     * @return string|Response
+     */
+    public function actionViewAsset($searchFilterVal = null, $workOrderID = null)
+    {
+        Yii::trace("CALL VIEW ASSET");
+        $model = new \yii\base\DynamicModel([
+            'modalSearch', 'mapGridSelected', 'sectionNumberSelected', 'pagesize'
+        ]);
+        $model->addRule('modalSearch', 'string', ['max' => 32])
+            ->addRule('mapGridSelected', 'string', ['max' => 32])
+            ->addRule('sectionNumberSelected', 'string', ['max' => 32])
+            ->addRule('pagesize', 'string', ['max' => 32]);
+
+        // Verify logged in
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/login']);
+        }
+
+        if (/*Yii::$app->request->get()*/$model->load(Yii::$app->request->queryParams)){
+            //todo: need to remove hard code value
+            $viewAssetFilterParams = $searchFilterVal;
+            $viewAssetPageSizeParams = 200;
+            //todo: need to replace above with following
+            //$viewAssetPageSizeParams = $model->pagesize;
+            //$pageAt = 1;
+            $pageAt = Yii::$app->getRequest()->getQueryParam('viewDispatchAssetPageNumber');
+            Yii::trace('PAGE AT : '.$pageAt);
+        }else{
+            $viewAssetFilterParams = "";
+            $viewAssetPageSizeParams = 200;
+            $pageAt = 1;
+        }
+
+        $getUrl = 'cge%2Fget-history&' . http_build_query([
+                'workOrderID' => $workOrderID,
+                'filter' => $viewAssetFilterParams,
+                'listPerPage' => $viewAssetPageSizeParams,
+                'page' => $pageAt,
+            ]);
+        $getHistoryDataResponse = json_decode(Parent::executeGetRequest($getUrl, Constants::API_VERSION_2), true); //indirect RBAC
+
+        Yii::trace("reGenerateAssetsData " . json_encode($getHistoryDataResponse));
+
+        // Put data in data provider
+        $assetDataProvider = new ArrayDataProvider
+        ([
+            'allModels' => $getHistoryDataResponse,
+            'pagination' => false,
+        ]);
+        $assetDataProvider->key = 'WorkOrderID';
+
+        //todo: set paging on both tables
+        // set pages to dispatch table
+        //$pages = new Pagination($getAssetDataResponse['pages']);
+
+        if (Yii::$app->request->isAjax) {
+            return $this->renderAjax('view_asset_modal', [
+                'assetDataProvider' => $assetDataProvider,
+                'model' => $model,
+                //'pages' => $pages,
+                'searchFilterVal' => $viewAssetFilterParams
+            ]);
+        } else {
+            return $this->render('view_asset_modal', [
+                'assetDataProvider' => $assetDataProvider,
+                'model' => $model,
+                //'pages' => $pages,
+                'searchFilterVal' => $viewAssetFilterParams
+            ]);
+        }
+    }
 }
