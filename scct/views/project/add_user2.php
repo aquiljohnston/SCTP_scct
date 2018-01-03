f228d853620b9e3ce6d40bfe897d9228e456a70b
Essex Igyan <eigyan@southerncrossinc.com>
2018-01-03 10:27:21 -0500

    SCC-326 - Added checkboxes for assign/unassign.

---
+++ scct/views/project/add_user2.php
@@ -0,0 +1,104 @@
+ <?php
+
+use yii\helpers\Html;
+use kartik\form\ActiveForm;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $project app\models\project */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Add Users to' . ' ' . $project->ProjectName;
+$this->params['breadcrumbs'][] = ['label' => 'Project', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $project->ProjectID, 'url' => ['view', 'id' => $project->ProjectID]];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="project-add-user" style="margin-top: 2%;">
+    <h1 class="title"><?= Html::encode($this->title) ?></h1>
+
+	<p>
+		<?= Html::a('Back', ['view' , 'id' => $project->ProjectID], ['class' => 'btn btn-primary']) ?>
+	</p>
+
+		<div class="row">
+			   <div id="unassignedTable">
+        <div id="unassignedTableGrid">
+   			<div class="col-sm-6">
+            <?= GridView::widget([
+                'id' 						=> 'unAssignedGV',
+                'dataProvider' 				=> $dataProviderUnassigned,
+                'export' 					=> false,
+                'pjax' 						=> false,
+                'floatHeader' 				=> true,
+                'summary' 					=> '',
+                'floatOverflowContainer' 	=> true,
+                'columns' => [
+
+                    [
+                        'label' 	=> 'Name',
+                        'attribute' => 'content',
+                    ],
+                    [
+                        'header' 			=> 'Assign User',
+                        'class' 			=> 'kartik\grid\CheckboxColumn',
+                        'contentOptions' 	=> ['class' => 'assignUser'],
+                        'checkboxOptions' 	=> function ($model, $key, $index, $column) {
+
+                            return ['userID' => $index,'disabled' => false];
+                        }
+                    ]
+                ]
+
+            ]  
+            
+              ); ?>
+          </div>
+        </div>
+    </div>
+
+    	   <div id="assignedTable">
+        <div id="assignedTableGrid">
+   			<div class="col-sm-6">
+            <?= GridView::widget([
+                'id' 				=> 'assignedGV',
+                'dataProvider' 		=> $dataProviderAssigned,
+                'export' 			=> false,
+                'pjax' 				=> false,
+                'floatHeader' 		=> true,
+                'summary' 			=> '',
+                'columns' 			=> [
+
+                    [
+                        'label' 	=> 'Name',
+                        'attribute' => 'content',
+                    ],
+                    [
+                        'header' 	=> 'Unassign User',
+                        'class' 	=> 'kartik\grid\CheckboxColumn',
+                        'contentOptions' => ['class' => 'assignUser'],
+                        'checkboxOptions' => function ($model, $key, $index, $column) {
+
+                            return ['userID' => $index,'disabled' => false,'checked'=>'checked'];
+                        }
+                    ]
+                ],
+                'floatOverflowContainer' => true,
+            ]); ?>
+          </div>
+        </div>
+    </div>
+
+
+
+
+		</div>
+		<input type="hidden" value=<?php echo $project->ProjectID;?> name="projectID" id="projectID">
+
+
+	<div class="form-group">
+		<?= Html::Button( 'Submit', ['class' => 'btn btn-success','id' => 'projectAddUserSubmitBtn']) ?>
+		<?= Html::resetButton('Reset', ['class' => 'btn btn-default','id' => 'projectAddUserResetBtn']) ?>
+	</div>
+</div>
