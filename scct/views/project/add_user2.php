eef84cedf54169b9342b76374d29e10b04da6d91
Essex Igyan <eigyan@southerncrossinc.com>
2018-01-04 15:57:44 -0500

    SCC-326 - User filters added to unassigned and assigned gridviews.

--- scct/views/project/add_user2.php
+++ scct/views/project/add_user2.php
@@ -23,10 +23,23 @@ $this->params['breadcrumbs'][] = $this->title;
 	</p>
 
 	<div class="row">
+         <?php $form = ActiveForm::begin([
+                'type' => ActiveForm::TYPE_VERTICAL,
+                'formConfig' => ['showLabels' => false,'deviceSize' => ActiveForm::SIZE_SMALL],
+                'options' => ['id' => 'projectForm']
+            ]); ?>
+        <label id="unassignedFilter" class="col-sm-6" style = "">
+            <?= $form->field($model, 'uaFilter')->textInput(['value' => $unassignedFilterParams, 'id' => 'projectFilter', 'style' => 'width:auto'])->label('Search'); ?>
+        </label>
+        <label id="assignedFilter" class="col-sm-6" style = "">
+            <?= $form->field($model, 'aFilter')->textInput(['value' => $assignedFilterParams, 'id' => 'projectFilter', 'style' => 'width:auto'])->label('Search'); ?>
+        </label>
+        <input type="hidden" value=<?php echo $project->ProjectID;?> name="projectID" id="projectID">
+    <?php ActiveForm::end(); ?>
 	</div>
 
 		<div class="row">
-     
+     <?php Pjax::begin(['id' => 'projectGridView', 'timeout' => false]) ?>
 		<div id="unassignedTable">
         <div id="unassignedTableGrid">
    			<div class="col-sm-6">
@@ -99,7 +112,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 		</div>
 		<input type="hidden" value=<?php echo $project->ProjectID;?> name="projectID" id="projectID">
- 
+    <?php Pjax::end() ?>
 	<div class="form-group">
 		<?= Html::Button( 'Submit', ['class' => 'btn btn-success','id' => 'projectAddUserSubmitBtn']) ?>
 		<?= Html::resetButton('Reset', ['class' => 'btn btn-default','id' => 'projectAddUserResetBtn']) ?>
