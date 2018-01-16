53c0af7bb5102beb6239b1e47d80707d399768fb
Essex Igyan <eigyan@southerncrossinc.com>
2018-01-16 16:08:47 -0500

    SCC-625 - Found that the issue was missing quotation marks around the input value for billing code.

--- scct/constants/Constants.php
+++ scct/constants/Constants.php
@@ -17,8 +17,9 @@ final class Constants
     const SERVER_PRODUCTION = '';
 	
 	//api url for different environments
-	const API_LOCAL_URL = 'http://localhost:8080/index.php?r=';
-	const API_DEV_URL = 'http://apidev.southerncrossinc.com/index.php?r=';
+	const API_LOCAL_URL = 'http://localhost:8888/index.php?r=';
+	//const API_DEV_URL = 'http://apidev.southerncrossinc.com/index.php?r=';
+	const API_DEV_URL = 'http://localhost:8888/index.php?r=';
 	const API_STAGE_URL = 'http://apistage.southerncrossinc.com/index.php?r=';
 	const API_PROD_URL = 'http://api.southerncrossinc.com/index.php?r=';
 	
