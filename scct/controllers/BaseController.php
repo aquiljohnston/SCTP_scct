f68eda6566f6d4d5cd7f143c93b2701e8676fc57
Michael Davis <mdavis@southerncrossinc.com>
2018-12-13 09:37:07 -0500

    Added support for new isolated demo environment. SCC-1139

--- scct/controllers/BaseController.php
+++ scct/controllers/BaseController.php
@@ -41,8 +41,11 @@ class BaseController extends Controller
 			return Constants::API_DEV_URL . "$version%2F$path";
 		} else if(strcmp($_SERVER['SERVER_PORT'], Constants::WEB_WITH_LOCAL_API_PORT) === 0) {
 			return Constants::API_LOCAL_URL . "$version%2F$path";
-			//Todo: review check, checks for demo in dev check because name does not follow the standard convention
-		} else if(strpos($prefix, Constants::SERVER_DEV) !== false || strpos($prefix, 'demo') !== false) {
+		} else if(strcmp($_SERVER['SERVER_PORT'], Constants::WEB_WITH_LOCAL_DEMO_API_PORT) === 0) {
+			return Constants::API_LOCAL_DEMO_URL . "$version%2F$path";
+		} else if(strpos($prefix, Constants::SERVER_DEMO) !== false){
+			return Constants::API_DEMO_URL . "$version%2F$path";
+		} else if(strpos($prefix, Constants::SERVER_DEV) !== false) {
             return Constants::API_DEV_URL . "$version%2F$path";
         } else if(strpos($prefix, Constants::SERVER_STAGE) !== false){
             return Constants::API_STAGE_URL . "$version%2F$path";
@@ -69,7 +72,11 @@ class BaseController extends Controller
                 || strpos($_SERVER['SERVER_NAME'],'192.168.')===0)
 		) 
 		{
-            return "scctdev";
+			if(strcmp($_SERVER['SERVER_PORT'], Constants::WEB_WITH_LOCAL_DEMO_API_PORT) === 0){
+				return "demo";
+			}else{
+				return "scctdev";
+			}
         }
         else {
             return self::urlPrefix();
