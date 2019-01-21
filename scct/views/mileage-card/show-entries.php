8efae9d1d1f70a1645cb0b502b315ef2154263f5
Michael Davis <mdavis@speardom.com>
2019-01-21 16:06:02 -0500

    Created new mileage card show entries screen, displays miles by task and day. Allows card approval and deactivation of entries. TODO add mileage modal SCC-1228

---
+++ scct/views/mileage-card/show-entries.php
@@ -0,0 +1,134 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use kartik\grid\GridView;
+use yii\bootstrap\Modal;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\mileage-card */
+?>
+<style type="text/css">
+[data-key="0"] 
+{
+    display:none;
+}
+</style>
+
+<div class="mileage-card-entries">
+
+    <input id="SundayDate" type="hidden" name="SundayDate" value=<?php echo $SundayDateFull; ?>>
+    <input id="SaturdayDate" type="hidden" name="SaturdayDate" value=<?php echo $SaturdayDateFull; ?>>
+    <input id="MileageCardProjectID" type="hidden" name="MileageCardProjectID" value=<?php echo $mileageCardProjectID; ?>>
+  
+    <div class="lightBlueBar">
+		<h3> <?= $projectName.' Week '.$from.' - '.$to.': '.$lName.', '.$fName; ?></h3>
+
+		<?php
+		$isAccountant = Yii::$app->session['UserAppRoleType'] == 'Accountant';
+		if ($model['MileageCardApprovedFlag'] == 1) {
+			$approve_status = true;
+		} else {
+			$approve_status = false;
+		}
+		?>
+		<p>
+			<?= Html::a('Back', ['index'], ['class' => 'btn btn-primary']) ?>
+			<?= Html::button('Approve', [
+				'class' => 'btn btn-primary',
+				'disabled' => $approve_status || $isAccountant,
+				'id' => 'approve_mileageCard_btn_id',
+			]) ?>
+			<?= Html::button('Deactivate', [
+				'class' => 'btn btn-primary',
+				'disabled' => true,
+				'id' => 'deactive_mileageEntry_btn_id',
+			]) ?>
+			<?= Html::button('Add Mileage', [
+				'class' => 'btn btn-primary add_mileageEntry_btn',
+				'disabled' => $approve_status && ($isSubmitted || !$isAccountant),
+				'id' => 'add_mileage_btn_id',
+			]) ?>
+		</p>
+		<br>
+    </div>
+    <?php Pjax::begin(['id' => 'ShowMileageEntriesView', 'timeout' => false]) ?>
+		<?= \kartik\grid\GridView::widget([
+			'id' => 'allMileageEntries',
+			'dataProvider' => $task,
+			'export' => false,
+			'pjax' => true,
+			'summary' => '',
+			'caption' => "",
+			'columns' => [
+				[
+					'label' => 'Task',
+					'attribute' => 'Task',
+				],
+				[
+					'label' => 'Sunday ' . $SundayDate,
+					'attribute' => 'Date1',
+					'headerOptions' => ['class'=>$SundayDateFull]
+				],
+				[
+					'label' => 'Monday '. $MondayDate,
+					'attribute' => 'Date2',
+					'headerOptions' => ['class'=>$MondayDateFull],
+				],
+				[
+					'label' => 'Tuesday '. $TuesdayDate,
+					'attribute' => 'Date3',
+					'headerOptions' => ['class'=>$TuesdayDateFull],
+				],
+				[
+					'label' => 'Wednesday '. $WednesdayDate,
+					'attribute' => 'Date4',
+					'headerOptions' => ['class'=>$WednesdayDateFull],
+				],
+				[
+					'label' => 'Thursday '. $ThursdayDate,
+					'attribute' => 'Date5',
+					'headerOptions' => ['class'=>$ThursdayDateFull],
+				],
+				[
+					'label' => 'Friday '. $FridayDate,
+					'attribute' => 'Date6',
+					'headerOptions' => ['class'=>$FridayDateFull],
+				],
+				[
+					'label' => 'Saturday '. $SaturdayDate,
+					'attribute' => 'Date7',
+					'headerOptions' => ['class'=>$SaturdayDateFull],
+				],
+				[
+					'header'            => 'Deactivate Mileage',
+					'class'             => 'kartik\grid\CheckboxColumn',
+					'contentOptions'    => [],
+					'checkboxOptions'   => function ($model) {
+						return ['mileageCardId' => Yii::$app->getRequest()->getQueryParam('id'),
+							'taskName' => $model['Task'],'entry' => '','class'=>'entryData'];
+					}
+				]
+			]
+		]);
+		?>
+		<?= Html::label('Total Miles: '. $model['SumMiles'],
+			null, ['id' => 'task_sum_hours']) ?>
+		<input type="hidden" value=<?php echo $model["MileageCardID"]?> name="mileageCardId" id="mileageCardId">
+		<input type="hidden" value=<?php echo $isAccountant ?> id="isAccountant">
+		<input type="hidden" value=<?php echo $isSubmitted ?> id="isSubmitted">
+    <?php Pjax::end() ?>
+    <?php
+    Pjax::begin(['id' => 'showMiles', 'timeout' => false]);
+		Modal::begin([
+			'header' => '<h4>ADD MILES</h4>',
+			'id' => 'addMilesModal',
+			'size' => 'modal-lg',
+		]);
+		echo "<div id='modalAddMiles'><span id='modalContentSpan'></span></div>";
+		Modal::end();
+    Pjax::end();
+    ?>
+
+</div>
